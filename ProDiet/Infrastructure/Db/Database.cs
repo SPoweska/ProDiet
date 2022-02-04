6c8d754994a778cc449708deea7bcc110834f0a2
Sebastian Poweska <spoweska98@gmail.com>
2022-02-04 15:11:17 +0100

    Project creation

---
+++ ProDiet/Infrastructure/Db/Database.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Db
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions options ) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is AuditableEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+
+            //var userName = ;
+
+            foreach (var entityEntry in entries)
+            {
+                ((AuditableEntity)entityEntry.Entity).ModifiedAt = DateTime.UtcNow;
+                //((AuditableEntity)entityEntry.Entity).ModifiedBy = userName;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((AuditableEntity)entityEntry.Entity).CreatedAt = DateTime.UtcNow;
+                    //((AuditableEntity)entityEntry.Entity).CreatedBy = userName;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ ProDiet/Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly Database db;
+
+        public UserRepository(Database db)
+        {
+            this.db = db;
+        }
+        public async Task CreateUser(User user)
+        {
+            if(user == null)
+            {
+                throw new Exception(nameof(User));
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync(default);
+        }
+    }
+}
